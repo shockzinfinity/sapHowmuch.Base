2c76af16133a3fc7261e0dd5a8f01d5771ae4556
Jun Yu <maddreamer@nate.com>
2017-04-07 09:50:20 +0900

    sap ui process check

--- sapHowmuch.Base.TestConsole/Program.cs
+++ sapHowmuch.Base.TestConsole/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Management;
 
 namespace sapHowmuch.Base.TestConsole
 {
@@ -9,6 +11,26 @@ namespace sapHowmuch.Base.TestConsole
 			try
 			{
 				Console.WriteLine(SapStream.DICompany.CompanyName);
+
+				//var sapProcess = ProcessHelper.ByName("SAP Business One.exe");
+
+				//foreach (var item in sapProcess)
+				//{
+				//	Console.WriteLine(item.Name);
+				//}
+				Console.WriteLine("Is connect to ui app? {0}", SapStream.IsConnectToUI);
+
+				if (SapStream.IsConnectToUI)
+				{
+					Console.WriteLine("IsHostedEnvironment: {0}", SapStream.UiApp.IsHostedEnvironment);
+					// NOTE: Company 에 로그인 되면 MetadataAutoRefresh 가 true 가 됨.
+					Console.WriteLine("MetadataAutoRefresh: {0}", SapStream.UiApp.MetadataAutoRefresh);
+
+					if (SapStream.UiApp.MetadataAutoRefresh)
+					{
+						SapStream.UiApp.MetadataAutoRefresh = false;
+					}
+				}
 			}
 			catch (Exception ex)
 			{
---
+++ sapHowmuch.Base/Extensions/ActionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace sapHowmuch.Base.Extensions
+{
+	public static partial class ActionExtensions
+	{
+		public static void Forward<T>(this T value, Action<T> function) => function(value);
+
+		public static void Forward<T1, T2>(this T1 value1, Action<T1, T2> function, T2 value2) => function(value1, value2);
+
+		public static void Forward<T1, T2, T3>(this T1 value1, Action<T1, T2, T3> function, T2 value2, T3 value3) => function(value1, value2, value3);
+
+		public static void Forward<T1, T2, T3, T4>(this T1 value1, Action<T1, T2, T3, T4> function, T2 value2, T3 value3, T4 value4) => function(value1, value2, value3, value4);
+	}
+}
