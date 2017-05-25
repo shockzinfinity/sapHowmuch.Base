810aa8d1b3c541f3bcf9176df897837a14191570
Jun Yu <shockz@iquest.co.kr>
2017-05-25 17:56:00 +0900

    sap ui event stream 생성

--- sapHowmuch.Base.TestConsole/Program.cs
+++ sapHowmuch.Base.TestConsole/Program.cs
@@ -28,9 +28,36 @@ namespace sapHowmuch.Base.TestConsole
 
 					if (SapStream.UiApp.MetadataAutoRefresh)
 					{
+						// SAP B1 클라이언트의 초기화면 (회사 연결 전) 에서는  MetadataAutoRefresh 가 false
+						// 회사에 로그인 되면 true 가 됨.
 						SapStream.UiApp.MetadataAutoRefresh = false;
+						Console.WriteLine($"MetadataAutoRefresh after login: {SapStream.UiApp.MetadataAutoRefresh}");
 					}
 				}
+
+				SapStream.AppEventStream.Subscribe(ev =>
+				{
+					Console.WriteLine($"[AppEvent] {ev.EventFiredTime.ToString("yyyy-MM-dd HH:mm:ss.fff")}");
+					Console.WriteLine($"EventType: {ev.DetailArg}");
+				});
+
+				SapStream.ItemStream.Subscribe(ev =>
+				{
+					Console.WriteLine($"[ItemEvent] {ev.EventFiredTime.ToString("yyyy-MM-dd HH:mm:ss.fff")}");
+					Console.WriteLine($"{ev.FormUid}, {ev.DetailArg.FormTypeEx}, {ev.DetailArg.ItemUID}, {ev.DetailArg.ColUID}");
+					Console.WriteLine($"Mode: {ev.DetailArg.FormMode}");
+					Console.WriteLine($"EventType: {ev.DetailArg.EventType} // {ev.DetailArg.BeforeAction} // {ev.DetailArg.ActionSuccess}");
+					Console.WriteLine($"ItemChanged: {ev.DetailArg.ItemChanged}");
+					Console.WriteLine($"Row: {ev.DetailArg.Row}");
+					Console.WriteLine($"InnerEvent: {ev.DetailArg.InnerEvent}");
+					Console.WriteLine($"Char: {ev.DetailArg.CharPressed} // Modifiers: {ev.DetailArg.Modifiers} // PopUpIndicator: {ev.DetailArg.PopUpIndicator}");
+				});
+
+				SapStream.MenuStream.Subscribe(ev =>
+				{
+					Console.WriteLine($"[MenuEvent] {ev.EventFiredTime.ToString("yyyy-MM-dd HH:mm:ss.fff")}");
+					Console.WriteLine($"{ev.DetailArg.BeforeAction}, {ev.DetailArg.MenuUID}, {ev.DetailArg.InnerEvent}");
+				});
 			}
 			catch (Exception ex)
 			{
---
+++ sapHowmuch.Base/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sapHowmuch.Base
+{
+	public static class Constants
+	{
+		public const string SapUiAppName = "SAP Business One.exe";
+		public const string SapUiDebugConnectionString = "0030002C0030002C00530041005000420044005F00440061007400650076002C0050004C006F006D0056004900490056";
+	}
+}
