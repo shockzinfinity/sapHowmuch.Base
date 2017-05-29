42c8c14a428ab1c4317c4d2f4007ec6a802fde94
Jun Yu <shockz@iquest.co.kr>
2017-05-29 11:13:11 +0900

    autofac 적용

---
+++ sapHowmuch.Base/DependencyResolver.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using sapHowmuch.Base.Interfaces;
+using System.ComponentModel.Composition;
+
+namespace sapHowmuch.Base
+{
+	[Export(typeof(IComponent))]
+	public class DependencyResolver : IComponent
+	{
+		#region IComponent implementation
+
+		public void Setup(IRegisterComponent registerComponent)
+		{
+			registerComponent.Builder.RegisterType<SapStream>()
+				.As<ISapStream>()
+				.PropertiesAutowired()
+				.InstancePerLifetimeScope();
+		}
+
+		#endregion IComponent implementation
+	}
+}
