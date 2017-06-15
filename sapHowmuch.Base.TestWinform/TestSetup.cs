dfcaf6c9269bf0519cb7afaf256278bc6a9a0146
Jun Yu <shockz@iquest.co.kr>
2017-06-15 17:12:16 +0900

    modify settings

--- sapHowmuch.Base.TestWinform/TestSetup.cs
+++ sapHowmuch.Base.TestWinform/TestSetup.cs
@@ -1,7 +1,6 @@
-﻿using sapHowmuch.Base.Constants;
-using sapHowmuch.Base.Helpers;
-using sapHowmuch.Base.Services;
+﻿using sapHowmuch.Base.Services;
 using sapHowmuch.Base.Setup;
+using System;
 
 namespace sapHowmuch.Base.TestWinform
 {
@@ -12,14 +11,16 @@ namespace sapHowmuch.Base.TestWinform
 		public void Run()
 		{
 			// for test
-			UserDefinedHelper.CreateTable("SHM_TestTbl1", "test table")
-				.CreateUdf("test_col1", "test column 1", SAPbobsCOM.BoFieldTypes.db_Alpha, 30)
-				.CreateUdf("test_col2", "test column 2", SAPbobsCOM.BoFieldTypes.db_Numeric, 10)
-				.CreateUdf("test_col3", "test column 3", SAPbobsCOM.BoFieldTypes.db_Numeric, 10);
+			//UserDefinedHelper.CreateTable("SHM_TestTbl1", "test table")
+			//	.CreateUdf("test_col1", "test column 1", SAPbobsCOM.BoFieldTypes.db_Alpha, 100)
+			//	.CreateUdf("test_col2", "test column 2", SAPbobsCOM.BoFieldTypes.db_Numeric, 10)
+			//	.CreateUdf("test_col3", "test column 3", SAPbobsCOM.BoFieldTypes.db_Numeric, 10);
 
-			UserDefinedHelper.CreateField(SboTable.BusinessPartner, "SHM_YesNo", "Yes or no?", SAPbobsCOM.BoFieldTypes.db_Alpha, 1, validValues: UserDefinedHelper.YesNoValidValues);
+			//UserDefinedHelper.CreateField(SboTable.BusinessPartner, "SHM_YesNo", "Yes or no?", SAPbobsCOM.BoFieldTypes.db_Alpha, 1, validValues: UserDefinedHelper.YesNoValidValues);
 
-			SettingService.Instance.InitSetting("sapHowmuch.savePath", "Save path", @"C:\temp\");
+			SettingService.Instance.InitSetting("sapHowmuch.appDataPath", "App data path", Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+
+			// 애드온 설치 위치, 세부 버전등 필요한 셋팅 값들은 여기서 컨트롤 한다.
 		}
 	}
 }
