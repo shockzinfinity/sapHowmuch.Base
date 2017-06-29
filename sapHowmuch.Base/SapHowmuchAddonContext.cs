931fe6c9ef34cc2974e57e88f28b714a1ec5e75b
Jun Yu <shockz@iquest.co.kr>
2017-06-29 14:11:13 +0900

    process priviliege 체크

--- sapHowmuch.Base/SapHowmuchAddonContext.cs
+++ sapHowmuch.Base/SapHowmuchAddonContext.cs
@@ -1,5 +1,9 @@
 ﻿using sapHowmuch.Base.Helpers;
+using sapHowmuch.Base.Management;
 using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Principal;
 using System.Windows.Forms;
 
 namespace sapHowmuch.Base
@@ -14,7 +18,16 @@ namespace sapHowmuch.Base
 				// 관지라로 실행되지 않았을 경우, ui api 연결에서 connection string 관련 에러가 발생한다.
 				// 고로, 프로세스 검증을 통해 관리자로 실행되었는지에 대한 검증이 필요하다.
 				// 해당 검증은 SapHowmuchAddonContext 에서 수행한다.
-				// TODO: 프로세스 권한 체크
+
+				var sapuiprocess = Process.GetProcessesByName(sapHowmuchConstants.SapUiAppName);
+				var result = UacHelper.IsElevatedOf(sapuiprocess.FirstOrDefault());
+
+				if (!result)
+				{
+					throw new Exception($"{sapHowmuchConstants.SapUiAppName} must be executed as [Administrator] privilieges.");
+				}
+
+				// TODO: 애드온 배포 이후에는 parent process 를 체크해야할 것으로 보임. sap client 에서 애드온 실행할 것이므로...
 
 				// setup loading 을 위해...
 				// 각 애드온 context 에서 로딩
